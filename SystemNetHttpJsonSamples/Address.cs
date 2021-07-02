4b10f77432aacc66e6faf8212f96781ac7acd1df
rafael sanchez port <rafysanchez@hotmail.com>
2021-07-02 10:06:01 -0300

    Add project files.

---
+++ SystemNetHttpJsonSamples/Address.cs
@@ -0,0 +1,10 @@
+﻿namespace SystemNetHttpJsonSamples
+{
+    public class Address
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Postcode { get; set; }
+        public Geo Geo { get; set; }
+    }
+}
